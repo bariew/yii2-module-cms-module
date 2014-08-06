cfa760061aa28b45d6a49fa61bbaab206e0ae5d0
Pavel Bariev <bariew@yandex.ru>
2014-08-06 19:18:06 +0400

    self-adding to packages

---
+++ ComposerInstaller.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pt
+ * Date: 8/6/14
+ * Time: 7:04 PM
+ */
+
+namespace bariew\moduleModule;
+
+
+use bariew\moduleModule\models\Package;
+use yii\composer\Installer;
+
+class ComposerInstaller extends Installer
+{
+    public function addModulePackage()
+    {
+        $package = new Package();
+        return parent::addPackage($package);
+    }
+} 
