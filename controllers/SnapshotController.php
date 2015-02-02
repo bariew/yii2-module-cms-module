9affc8fd9a768c71adc6e1c76d4d9783ba52d607
Pavel Bariev <bariew@yandex.ru>
2015-02-02 16:54:31 +0400

    added snapshot func

--- controllers/SnapshotController.php
+++ controllers/SnapshotController.php
@@ -3,32 +3,18 @@
 namespace bariew\moduleModule\controllers;
 
 use bariew\moduleModule\models\Composer;
+use bariew\moduleModule\models\Snapshot;
 use Yii;
 use yii\web\Controller;
 
 /**
- * ItemController implements the CRUD actions for Item model.
+ * SnapshotController implements the CRUD actions for Snapshot model.
  */
 class SnapshotController extends Controller
 {
-    /**
-     * Lists all Item models.
-     * @param bool $runComposer whether to run composer install/uninstall
-     * @return mixed
-     */
-    public function actionIndex($runComposer = true)
-    {
-        $model = new Composer();
-        if ($runComposer && $toInstall = Yii::$app->request->post('install')) {
-            $actions = [];
-            foreach ($toInstall as $name => $value) {
-                $actions[$value][] = $name;
-            }
-            $model::removeAll(@$actions[Composer::ACTION_REMOVE]);
-            $model::requireAll(@$actions[Composer::ACTION_INSTALL]);
-            $model::updateAll(@$actions[Composer::ACTION_UPDATE]);
-        }
 
-        return $this->render('index', compact('model'));
+    public function actionIndex()
+    {
+        return (new Snapshot())->compact();
     }
 }
--- models/Snapshot.php
+++ models/Snapshot.php
@@ -10,22 +10,29 @@ namespace bariew\moduleModule\models;
 
 use bariew\moduleModule\Module;
 use yii\console\controllers\MigrateController;
+use yii\db\Query;
 
 class Snapshot
 {
     private $paths = [
-        '@app/config/local/main.php',
-        '@app/migrations',
-        '@app/composer.json',
-        '@app/composer.lock',
-        '@app/web/themes'
+        'localConfig'  => '@app/config/local/main.php',
+        'migrations'   => '@app/migrations',
+        'modules'      => '@app/modules',
+        'composerJson' => '@app/composer.json',
+        'composerLock' => '@app/composer.lock',
+        'themes'       => '@app/web/themes',
     ];
+
+    private $archivePath = '@app/runtime/snapshot.zip';
 
     public function compact()
     {
-        $this->createMigrations();
-        $this->createArchive($this->paths);
-        $this->sendArchive();
+        $path = \Yii::getAlias($this->archivePath);
+        $this->createMigrations()
+            ->createArchive($this->paths);
+        \Yii::$app->response->sendFile($path, 'snapshot_' . date('Y-m-d') . '.zip');
+        unlink($path);
+        \Yii::$app->end();
     }
 
     public function extract()
@@ -34,5 +41,69 @@ class Snapshot
         Composer::installAll();
         $controller = new MigrateController('migrate', new Module('module'));
         $controller->actionUp();
+    }
+
+    private function createMigrations()
+    {
+        $result = [];
+        foreach (\Yii::$app->db->schema->getTableNames() as $table) {
+            if ($table == 'migration') {
+                continue;
+            }
+            $result[$table] = (new Query())->from([$table])->all();
+        }
+        $name = 'm' . gmdate('ymd_His') . '_snapshot';
+        $file = \Yii::getAlias($this->paths['migrations']) . DIRECTORY_SEPARATOR . $name . '.php';
+        $content = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'snapshot_migration_template.php');
+        $content = str_replace(['{{name}}', '{{data}}'], [$name, var_export($result, true)], $content);
+        file_put_contents($file, '<?php ' . $content);
+        return $this;
+    }
+
+    /**
+     * @param $paths
+     * @return $this
+     */
+    private function createArchive($paths)
+    {
+        $zip = new \ZipArchive();
+        $zip->open(\Yii::getAlias($this->archivePath), \ZipArchive::CREATE);
+        foreach ($paths as $alias) {
+            $path = \Yii::getAlias($alias);
+            if (is_file($path)) {
+                $innerPath = str_replace([
+                    \Yii::getAlias('@app'),
+                    DIRECTORY_SEPARATOR . basename($path)
+                ], ['', ''], $path);
+            } else {
+                $innerPath = '';
+            }
+            $this->zipRecursive($zip, $path, $innerPath);
+        }
+        return $this;
+    }
+
+
+    /**
+     * @param \ZipArchive $zip
+     * @param $src
+     * @param string $innerPath
+     * @return bool
+     */
+    private function zipRecursive(\ZipArchive $zip, $src, $innerPath = '')
+    {
+        $localName = $innerPath
+            ? $innerPath . DIRECTORY_SEPARATOR . basename($src)
+            : basename($src);
+        if (is_file($src)) {
+            return $zip->addFile($src, $localName);
+        }
+        $zip->addEmptyDir($localName);
+        foreach (scandir($src) as $file) {
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
+            $this->zipRecursive($zip, $src . DIRECTORY_SEPARATOR . $file, $localName);
+        }
     }
 } 
